4fb837d649d6d1a4e91fc7a5db65e1c66462f460
David Alberto Silva Solano <davidsilva3290@gmail.com>
2020-02-19 15:09:57 -0500

    Update: fasta algotithm

--- 03_springboot_rest_stress_testing/jmeter/src/main/java/com/demo/rest/jmeter/HomeController.java
+++ 03_springboot_rest_stress_testing/jmeter/src/main/java/com/demo/rest/jmeter/HomeController.java
@@ -17,8 +17,8 @@ class HomeControler {
   @GetMapping("/test1")
   public ResponseEntity<String> test1() {
     try {
-      Binarytrees binarytrees = new Binarytrees();
-      binarytrees.InitBenchmark();
+      Fasta fasta = new Fasta();
+      fasta.InitBenchmark();
     } catch (Exception e) {
       System.out.println(e.getMessage());
       return new ResponseEntity<>("Unknown error", new HttpHeaders(), HttpStatus.BAD_REQUEST);
